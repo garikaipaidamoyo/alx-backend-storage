eb2d5a313ba4827ddf85559a62f1ec228db52fc1
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-10-18 10:48:29 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-
 import redis
 import uuid
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -12,17 +8,41 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable] = None):
+        data = self._redis.get(key)
+        if data is not None and fn:
+            return fn(data)
+        return data
 
-cache = Cache()
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
+
+    # Task 1: Reading from Redis and recovering original type
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
