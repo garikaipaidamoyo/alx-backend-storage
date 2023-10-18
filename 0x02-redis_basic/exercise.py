6ed288e26858b6880ecbc4e459ff166a78a03c0f
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-10-18 10:55:13 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 import redis
 import uuid
+from functools import wraps
 from typing import Callable, Optional, Union
 
 
@@ -25,6 +26,48 @@ class Cache:
     def get_int(self, key: str) -> int:
         return self.get(key, fn=int)
 
+    def count_calls(fn: Callable):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            key = fn.__qualname__
+            self._redis.incr(key)
+            return fn(self, *args, **kwargs)
+        return wrapper
+
+    def call_history(fn: Callable):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            inputs_key = f"{key}:inputs"
+            outputs_key = f"{key}:outputs"
+            self._redis.rpush(inputs_key, str(args))
+            output = fn(self, *args, **kwargs)
+            self._redis.rpush(outputs_key, output)
+            return output
+        return wrapper
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def replay(fn: Callable):
+        def wrapper(self):
+            call_count = self._redis.get(fn.__qualname__)
+            if call_count:
+                call_count = int(call_count)
+                print(f"{fn.__qualname__} was called {call_count} times:")
+                inputs_key = f"{fn.__qualname__}:inputs"
+                outputs_key = f"{fn.__qualname__}:outputs"
+                inputs = self._redis.lrange(inputs_key, 0, call_count - 1)
+                outputs = self._redis.lrange(outputs_key, 0, call_count - 1)
+                for i, (inp, out) in enumerate(zip(inputs, outputs), start=1):
+                    print(f"{fn.__qualname__}{inp} -> {out.decode()}")
+            else:
+                print(f"{fn.__qualname__} was not called.")
+        return wrapper
+
 
 if __name__ == "__main__":
     cache = Cache()
@@ -46,3 +89,9 @@ if __name__ == "__main__":
     for value, fn in TEST_CASES.items():
         key = cache.store(value)
         assert cache.get(key, fn=fn) == value
+
+    # Task 4: Retrieving lists
+    cache.store("foo")
+    cache.store("bar")
+    cache.store(42)
+    cache.replay(cache.store)
